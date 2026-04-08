8bbc92aa0f3d91b5adf995190b75ff7b15f858ca
Hexcoder <rbnelr@gmail.de>
2026-04-08 13:14:35 +0200

    very clever optimization and it actually works well, could probably be optimized further

--- control.lua
+++ control.lua
@@ -1,5 +1,9 @@
 --[[
 	TODO: add nixie tube style mode?
+	TODO: I think there's now some bugs with copy pasting, possibly only directly selecting signals for some reason?
+	TODO: maybe add the option with braces required, as this would ensures undo/redo, blueprint over etc. to all work 100% as expected
+	      trade off is that it's slightly uglier, but code is easier as well
+		  -> or just revert these changes
 ]]
 
 ---@type ModStorage
@@ -15,6 +19,13 @@ storage = storage
 ---@field entity LuaEntity
 ---@field set_convenient_condition_once? boolean -- set condition conveniently once, set to nil afterwards
 ---@field unmodified_messages DisplayPanelMessageDefinition[]
+---@field acs LuaEntity[]
+
+local W = defines.wire_connector_id
+local HIDDEN = defines.wire_origin.script
+--local HIDDEN = defines.wire_origin.player
+local CR = defines.wire_connector_id.circuit_red
+local CG = defines.wire_connector_id.circuit_green
 
 -- example tich text:
 -- [virtual-signal=signal-deny] [entity=big-biter]  [virtual-signal=signal-B]
@@ -32,6 +43,14 @@ local SIGNAL2RICH_TEXT = {
 }
 local ANY_SIGNAL_COND = { first_signal={type="virtual", name="signal-anything"}, constant=0, comparator="≠" }
 
+local AC_NEGATE_EACH_R = { ---@type ArithmeticCombinatorParameters
+	first_signal={type="virtual", name="signal-each"}, second_constant=-1, operation="*", first_signal_networks={green=false},
+	output_signal={type="virtual", name="signal-each"}
+}
+local AC_NEGATE_EACH_G = { ---@type ArithmeticCombinatorParameters
+	first_signal={type="virtual", name="signal-each"}, second_constant=-1, operation="*", first_signal_networks={red=false},
+	output_signal={type="virtual", name="signal-each"}
+}
 
 --local function migrate()
 --	for _,data in pairs(storage.tracked_displays) do
@@ -61,18 +80,21 @@ local function stop_tracking(display)
 	local id = display.unit_number ---@cast id -nil
 	local data = storage.tracked_displays[id]
 	if data then
+		for _,ac in pairs(data.acs) do
+			ac.destroy()
+		end
+		
 		local ctrl = display.get_or_create_control_behavior() --[[@as LuaDisplayPanelControlBehavior?]]
 		if ctrl then
 			ctrl.messages = data.unmodified_messages
 		end
-		
-		storage.tracked_displays[id] = nil
 		
 		local surf = storage.surfaces[display.surface_index]
 		if surf then
 			surf.chart[id] = nil
 			surf.alt[id] = nil
 		end
+		storage.tracked_displays[id] = nil
 	end
 end
 -- start or stop updating depending on if any messages contain {}
@@ -86,37 +108,28 @@ local function update_tracking(display, copy_from)
 	if copy_from then
 		local data_src = storage.tracked_displays[copy_from.unit_number]
 		if data_src then
-			script.register_on_object_destroyed(display)
-			
 			-- copy control behavior
 			ctrl = display.get_or_create_control_behavior() --[[@as LuaDisplayPanelControlBehavior]]
 			if ctrl then
 				ctrl.messages = data_src.unmodified_messages
 			end
+		end
+	end
+	
+	ctrl = ctrl or display.get_control_behavior() --[[@as LuaDisplayPanelControlBehavior]]
+	if copy_from or (ctrl and has_any_trigger_message(ctrl)) then
+		data = storage.tracked_displays[id]
+		if not data then
+			script.register_on_object_destroyed(display)
 			
-			-- start tracking because copy_from was also tracked
-			local data = {
+			data = {
 				entity = display, sid = display.surface_index,
 				unmodified_messages = ctrl.messages
 			}
 			storage.tracked_displays[id] = data
 		end
 	else
-		ctrl = display.get_control_behavior() --[[@as LuaDisplayPanelControlBehavior]]
-		if ctrl and has_any_trigger_message(ctrl) then
-			data = storage.tracked_displays[id]
-			if not data then
-				script.register_on_object_destroyed(display)
-				
-				data = {
-					entity = display, sid = display.surface_index,
-					unmodified_messages = ctrl.messages
-				}
-				storage.tracked_displays[id] = data
-			end
-		else
-			stop_tracking(display)
-		end
+		stop_tracking(display)
 	end
 	
 	-- update update lists
@@ -127,6 +140,33 @@ local function update_tracking(display, copy_from)
 		surf.alt[id] = display.display_panel_always_show and data or nil
 		
 		storage.surfaces[display.surface_index] = surf
+		
+		if not data.acs then
+			local function make_combinator(x,y, params)
+				local ac = display.surface.create_entity{
+					name="hexcoder-signal-display-ac", force=display.force,
+					position={display.position.x+x, display.position.y+y}, snap_to_grid=false,
+					direction=defines.direction.north
+				} ---@cast ac -nil
+				ac.destructible = false
+				ac.get_or_create_control_behavior().parameters = params
+				return ac
+			end
+			
+			data.acs = {
+				make_combinator(-0.4, -1.5, AC_NEGATE_EACH_R),
+				make_combinator(0.4, -1.5, AC_NEGATE_EACH_G),
+			}
+			
+			local acR = data.acs[1].get_wire_connectors(true)
+			local acG = data.acs[2].get_wire_connectors(true)
+			local d = display.get_wire_connectors(true)
+			
+			acR[W.combinator_input_red  ].connect_to(d[W.circuit_red], false, HIDDEN)
+			acR[W.combinator_input_green].connect_to(acR[W.combinator_output_green], false, HIDDEN)
+			acG[W.combinator_input_red  ].connect_to(acG[W.combinator_output_red], false, HIDDEN)
+			acG[W.combinator_input_green].connect_to(d[W.circuit_green], false, HIDDEN)
+		end
 	end
 end
 
@@ -135,6 +175,10 @@ script.on_event(defines.events.on_object_destroyed, function(event)
 		local id = event.useful_id
 		local data = storage.tracked_displays[id]
 		if data then
+			for _,ac in pairs(data.acs) do
+				ac.destroy()
+			end
+			
 			local surf = storage.surfaces[data.sid]
 			if surf then
 				surf.chart[id] = nil
@@ -169,9 +213,22 @@ local function _comp_signal(l, r)
 	return l.count > r.count
 end
 
+local dbg = true
+local function _dbg_update(display)
+	local pos = display.position
+	
+	rendering.draw_line{ from={ x=pos.x-0.45, y=pos.y-0.45 }, to={ x=pos.x+0.45, y=pos.y+0.45 }, color={.2,1,.2}, width=2, surface=display.surface, time_to_live=1 }
+	rendering.draw_line{ from={ x=pos.x-0.45, y=pos.y+0.45 }, to={ x=pos.x+0.45, y=pos.y-0.45 }, color={.2,1,.2}, width=2, surface=display.surface, time_to_live=1 }
+	
+	rendering.draw_line{ from={ x=pos.x-0.45, y=pos.y-0.45 }, to={ x=pos.x+0.45, y=pos.y+0.45 }, color={.2,1,.2}, width=2, surface=display.surface, time_to_live=1, render_mode="chart" }
+	rendering.draw_line{ from={ x=pos.x-0.45, y=pos.y+0.45 }, to={ x=pos.x+0.45, y=pos.y-0.45 }, color={.2,1,.2}, width=2, surface=display.surface, time_to_live=1, render_mode="chart" }
+end
+
+local _changed
+
 ---@param display LuaEntity
 ---@param data DisplayData
-local function update(display, data)
+local function update(display, data, newly_revealed)
 	-- display.display_panel_text and display.display_panel_icon
 	-- are essentially variables that the player can config if the display is not connect to circuits
 	-- when the display is connected to circuits they can configure circuit conditions
@@ -192,6 +249,15 @@ local function update(display, data)
 	-- for example so that if the user uses multiple messages with conditions if all of them try to show the same signals, I only compute them once
 	-- however I don't tend to use this so I don't bother for now
 	-- I guess caching by actually just not updating displays with unchanged signals would be far better?
+	
+	-- Cool optimization: edge detector AC, read combined inputs (current + negated_previous tick signals) if get_signals()=nil, then no change
+	-- TODO: could try cache if R and G are actually connected and avoid one of these reads most of the time
+	-- newly_revealed: Need to track which checks were skipped by player optimization, since we might miss changes older that last tick
+	if not newly_revealed and data.acs[1].get_signals(CR, CG) == nil and data.acs[2].get_signals(CR, CG) == nil then
+		return
+	end
+	
+	--if dbg then _dbg_update(display) end
 	
 	-- cache in case multiple messages exist
 	local all_signals = nil
@@ -313,16 +379,26 @@ local function update(display, data)
 			})
 		end
 	end
+	
+	--_changed = _changed + 1
 end
 
+local seen_last_tick = {}
+
+--[[
 local function optimized_update()
 	local margin = 1.10 -- at least try to catch stuff that is slightly off-screen
+	--local margin = 0.6 -- debugging
 	-- unlike many games, zoom is not tied to vertical or horizontal fov/orthographic diameter
 	-- instead at zoom=1 I get 1 tile being drawn at 32 pixels at my specific window resolution, 2 is zoomed in to 1 tile=64
 	local factor = margin*0.5/32
 	
 	-- do not update display twice
 	local updated = {}
+	
+	--local _in_update_list = 0
+	--local _updated = 0
+	--_changed = 0
 	
 	for _, player in pairs(game.players) do
 		if not player.connected then goto continue end
@@ -347,10 +423,10 @@ local function optimized_update()
 			local x1 = player.position.x + half_sizeX + 1
 			local y0 = player.position.y - half_sizeY - 1
 			local y1 = player.position.y + half_sizeY + 1
-			--rendering.draw_line{ from={ x=x0, y=y0 }, to={ x=x0, y= y1 }, color={.2,1,.2}, width=4, surface=player.surface, time_to_live=1 }
-			--rendering.draw_line{ from={ x=x1, y=y0 }, to={ x=x1, y= y1 }, color={.2,1,.2}, width=4, surface=player.surface, time_to_live=1 }
-			--rendering.draw_line{ from={ x=x0, y=y0 }, to={ x=x1, y= y0 }, color={.2,1,.2}, width=4, surface=player.surface, time_to_live=1 }
-			--rendering.draw_line{ from={ x=x0, y=y1 }, to={ x=x1, y= y1 }, color={.2,1,.2}, width=4, surface=player.surface, time_to_live=1 }
+			--rendering.draw_line{ from={ x=x0, y=y0 }, to={ x=x0, y= y1 }, color={1,.2,.2}, width=4, surface=player.surface, time_to_live=1 }
+			--rendering.draw_line{ from={ x=x1, y=y0 }, to={ x=x1, y= y1 }, color={1,.2,.2}, width=4, surface=player.surface, time_to_live=1 }
+			--rendering.draw_line{ from={ x=x0, y=y0 }, to={ x=x1, y= y0 }, color={1,.2,.2}, width=4, surface=player.surface, time_to_live=1 }
+			--rendering.draw_line{ from={ x=x0, y=y1 }, to={ x=x1, y= y1 }, color={1,.2,.2}, width=4, surface=player.surface, time_to_live=1 }
 			
 			for _, data in pairs(update_list) do
 				local pos = data.entity.position
@@ -358,18 +434,24 @@ local function optimized_update()
 				local y = pos.y
 				if x > x0 and x < x1 and y > y0 and y < y1 then
 					if data and not updated[data] then
-						update(data.entity, data)
+						update(data.entity, data, seen_last_tick[data] == nil)
 						updated[data] = true
+	--
+						--_updated = _updated + 1
 					end
 				end
+				
+				--_in_update_list = _in_update_list + 1
 			end
 		end
 		
 		function _update(display)
 			local data = display and display.valid and storage.tracked_displays[display.unit_number] or nil
 			if data and not updated[data] then
-				update(display, data)
+				update(display, data, seen_last_tick[data] == nil)
 				updated[data] = true
+				
+				--_updated = _updated + 1
 			end
 		end
 		
@@ -381,8 +463,62 @@ local function optimized_update()
 		--	_update(player.opened) -- open gui
 		--end
 		
+		-- update chart or alt mode displays unless player is in non-alt mode
+		if update_list then
+			for _, data in pairs(update_list) do
+				update(data.entity, data, false)
+			end
+		end
+		
+		-- update always if hovered
+		local sel = player.selected
+		local data = sel and sel.valid and storage.tracked_displays[sel.unit_number] or nil
+		if data and not update_list[data] then
+			update(sel, data, false)
+		end
+		
 		::continue::
 	end
+	
+	seen_last_tick = updated
+end
+]]
+
+-- TODO: fix this version for multiple players: need to determine active surfaces then only iterate list for surface once
+local function optimized_update()
+	for _, player in pairs(game.players) do
+		if not player.connected then goto continue end
+		
+		local surf = storage.surfaces[player.surface_index]
+		
+		-- update chart or alt mode displays unless player is in non-alt mode
+		local update_list ---@type table<unit_number, DisplayData>
+		if surf then
+			if player.render_mode == defines.render_mode.chart then -- game view or char_zoomed_in render actual entities, chart is map mode
+				update_list = surf.chart
+			elseif player.game_view_settings.show_entity_info then
+				update_list = surf.alt
+			end
+		end
+		
+		-- update chart or alt mode displays unless player is in non-alt mode
+		if update_list then
+			for _, data in pairs(update_list) do
+				update(data.entity, data, false)
+			end
+		end
+		
+		-- update always if hovered
+		local sel = player.selected
+		local data = sel and sel.valid and storage.tracked_displays[sel.unit_number] or nil
+		if data and not (update_list and update_list[data]) then
+			update(sel, data, false)
+		end
+		
+		::continue::
+	end
+	
+	seen_last_tick = updated
 end
 
 script.on_nth_tick(12, function(event)
@@ -467,6 +603,8 @@ script.on_event(defines.events.on_player_setup_blueprint, function(event)
 	end
 end)
 
+-- undo-redo placing displays causes them to copy displayed text, not formatting text
+
 local function init(clean)
 	storage.tracked_displays = {}
 	storage.surfaces = {}
@@ -482,6 +620,14 @@ end
 local function _reset()
 	for _,data in pairs(storage.tracked_displays) do
 		stop_tracking(data.entity)
+	end
+	
+	for _, s in pairs(game.surfaces) do
+		for _, name in pairs({"ac"}) do
+			for _, e in pairs(s.find_entities_filtered{ name="hexcoder-signal-display-"..name }) do
+				e.destroy()
+			end
+		end
 	end
 
 	storage = {}
---
+++ data-updates.lua
@@ -0,0 +1,67 @@
+mod_name = "hexcoder-signal-display-"
+local dbg = false
+
+local function make_phantom(thing)
+	thing.flags = {"not-on-map",
+		"not-rotatable", "not-flammable", "not-repairable",
+		"not-deconstructable", "not-blueprintable", "no-copy-paste", "not-upgradable",
+		"not-in-kill-statistics", "not-in-made-in",
+		"no-automated-item-removal", "no-automated-item-insertion"
+	}
+	
+	thing.hidden = true
+	thing.minable = {minable=false, mining_time=999999}
+	thing.corpse = nil
+	thing.dying_explosion = nil
+	thing.collision_box = nil
+	thing.collision_mask = { layers = {} }
+	thing.damaged_trigger_effect = nil
+	thing.fast_replaceable_group = nil
+	thing.open_sound = nil
+	thing.close_sound = nil
+	thing.impact_category = nil
+	
+	if dbg then
+		thing.selection_priority = 100
+	else
+		table.insert(thing.flags, "hide-alt-info")
+		
+		thing.picture = nil
+		thing.sprites = nil
+		
+		thing.selection_box = {{0,0}, {0,0}}
+		thing.selectable_in_game = false
+		
+		thing.draw_circuit_wires = false
+		
+		-- CC
+		thing.activity_led_light = nil
+		thing.activity_led_sprites = nil
+		-- DC
+		thing.equal_symbol_sprites = nil
+		thing.greater_symbol_sprites = nil
+		thing.less_symbol_sprites = nil
+		thing.greater_or_equal_symbol_sprites = nil
+		thing.less_or_equal_symbol_sprites = nil
+		thing.not_equal_symbol_sprites = nil
+		-- AC
+		thing.plus_symbol_sprites = nil
+		thing.minus_symbol_sprites = nil
+		thing.multiply_symbol_sprites = nil
+		thing.divide_symbol_sprites = nil
+		thing.modulo_symbol_sprites = nil
+		thing.power_symbol_sprites = nil
+		thing.left_shift_symbol_sprites = nil
+		thing.right_shift_symbol_sprites = nil
+		thing.and_symbol_sprites = nil
+		thing.or_symbol_sprites = nil
+		thing.xor_symbol_sprites = nil
+	end
+end
+
+local ac = util.table.deepcopy(data.raw["arithmetic-combinator"]["arithmetic-combinator"])
+ac.name = mod_name.."ac"
+ac.energy_source = { type = "void" }
+make_phantom(ac)
+
+data:extend({ac})
