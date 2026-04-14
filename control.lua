8f47c668e9ee08e06f6ac61ccc92fc53977a490f
Hexcoder <rbnelr@gmail.de>
2026-04-14 14:49:45 +0200

    optimzation stuff

--- control.lua
+++ control.lua
@@ -15,7 +15,9 @@ storage = storage
 ---@class Display
 ---@field entity DisplayEntity
 ---@field ctrl LuaDisplayPanelControlBehavior?
----@field acs? LuaEntity[]
+---@field acs? table<defines.wire_connector_id.circuit_red|defines.wire_connector_id.circuit_green, LuaEntity>
+---@field ac1 LuaEntity?
+---@field ac2 LuaEntity?
 ---@field sid integer
 ---@field poll_idx integer
 
@@ -24,10 +26,10 @@ storage = storage
 ---@field alt DisplaySet
 
 local W = defines.wire_connector_id
+local CR = W.circuit_red
+local CG = W.circuit_green
 local HIDDEN = defines.wire_origin.script
 --local HIDDEN = defines.wire_origin.player
-local CR = defines.wire_connector_id.circuit_red
-local CG = defines.wire_connector_id.circuit_green
 
 -- example tich text:
 -- [virtual-signal=signal-deny] [entity=big-biter]  [virtual-signal=signal-B]
@@ -54,6 +56,18 @@ local AC_NEGATE_EACH_G = { ---@type ArithmeticCombinatorParameters
 	output_signal={type="virtual", name="signal-each"}
 }
 
+local function _dbg_update(display, no_change)
+	local pos = display.position
+	
+	local color = no_change and {.2,.2,1} or {.2,1,.2}
+	
+	rendering.draw_line{ from={ x=pos.x-0.45, y=pos.y-0.45 }, to={ x=pos.x+0.45, y=pos.y+0.45 }, color=color, width=2, surface=display.surface, time_to_live=1 }
+	rendering.draw_line{ from={ x=pos.x-0.45, y=pos.y+0.45 }, to={ x=pos.x+0.45, y=pos.y-0.45 }, color=color, width=2, surface=display.surface, time_to_live=1 }
+	
+	rendering.draw_line{ from={ x=pos.x-0.45, y=pos.y-0.45 }, to={ x=pos.x+0.45, y=pos.y+0.45 }, color=color, width=2, surface=display.surface, time_to_live=1, render_mode="chart" }
+	rendering.draw_line{ from={ x=pos.x-0.45, y=pos.y+0.45 }, to={ x=pos.x+0.45, y=pos.y-0.45 }, color=color, width=2, surface=display.surface, time_to_live=1, render_mode="chart" }
+end
+
 -- TODO: should_update returns false if display not connected to circuit, but now it does not start updating on wire connect
 
 ---@param e LuaEntity?
@@ -77,17 +91,13 @@ local function remove_from_poll_list(data)
 	storage.poll_list[last] = nil
 end
 
----@param display DisplayEntity
+---@param c LuaWireConnector
 ---@returns boolean
 -- any circuit connection not going to hidden change detector
-local function is_actually_connected(display)
-	for _, c in pairs(display.get_wire_connectors(false)) do
-		if c.real_connection_count >= 2 then
-			return true
-		end
-		if c.real_connection_count >= 1 and c.real_connections[1].origin == defines.wire_origin.player then
-			return true
-		end
+local function is_actually_connected(c)
+	if c.real_connection_count >= 2 or
+	   c.real_connection_count >= 1 and c.real_connections[1].origin == defines.wire_origin.player then
+		return true
 	end
 	return false
 end
@@ -105,7 +115,7 @@ local function is_active_display(display, ctrl)
 	
 	-- need to return false if display not connected to circuit to avoid display updating despite no circuit
 	-- (and overwriting static user message since ctrl.messages still exists despite being hidden)
-	if display.valid and ctrl and is_actually_connected(display) then
+	if display.valid and ctrl then
 		for _,m in ipairs(ctrl.messages) do
 			if m.icon and m.text and m.text:find("{[^{}]*}") then
 				return true
@@ -139,20 +149,20 @@ local function reset_display(id, data)
 	-- reset messages even if not data yet, relevant for copy-pasted entities
 	reset_messages(data)
 	
-	if data.acs then
-		for _,ac in pairs(data.acs) do
-			ac.destroy()
-		end
-		data.acs = nil
-		
-		local surf = storage.surfaces[data.sid]
-		if surf then
-			surf.chart[id] = nil
-			surf.alt[id] = nil
-		end
-		
-		storage.extra_update[id] = nil
+	for _,ac in pairs(data.acs) do
+		ac.destroy()
 	end
+	data.acs = {}
+	data.ac1 = nil
+	data.ac2 = nil
+	
+	local surf = storage.surfaces[data.sid]
+	if surf then
+		surf.chart[id] = nil
+		surf.alt[id] = nil
+	end
+	
+	storage.extra_update[id] = nil
 end
 
 -- start or stop updating display depending on if any messages contain format trigger {}
@@ -163,9 +173,10 @@ local function check_display(display)
 	if not data then
 		script.register_on_object_destroyed(display)
 		
-		data = { ---@diagnostic disable-line
+		data = {
 			entity = display,
-			sid = display.surface_index
+			sid = display.surface_index,
+			acs = {}
 		}
 		add_to_poll_list(data)
 		storage.all_displays[id] = data
@@ -173,38 +184,60 @@ local function check_display(display)
 	
 	data.ctrl = display.get_control_behavior() --[[@as LuaDisplayPanelControlBehavior?]]
 	
-	if is_active_display(display, data.ctrl) then
-		local function make_combinator(x,y, params, detect_wire)
-			local ac = display.surface.create_entity{
+	local acs = data.acs
+	local active = is_active_display(display, data.ctrl)
+	
+	if active then
+		local conns = display.get_wire_connectors(false)
+		
+		local function update_combinator(wire)
+			local connected = is_actually_connected(conns[wire])
+			local ac = acs[wire]
+			if (ac ~= nil) == connected then
+				return false -- nothing to do
+			end
+			
+			if ac then
+				ac.destroy()
+				acs[wire] = nil
+				return true
+			end
+			
+			ac = display.surface.create_entity{
 				name="hexcoder-signal-display-hidden-change-detector", force=display.force,
-				position={display.position.x+x, display.position.y+y}, snap_to_grid=false,
+				position={display.position.x + (wire == W.circuit_red and -0.4 or 0.4), display.position.y-1.5}, snap_to_grid=false,
 				direction=defines.direction.north
 			} ---@cast ac -nil
 			ac.destructible = false
 			
 			local ctrl = ac.get_or_create_control_behavior() --[[@as LuaArithmeticCombinatorControlBehavior]]
-			ctrl.parameters = params
 			
 			local conn = ac.get_wire_connectors(true)
 			local d = display.get_wire_connectors(true)
 			
-			if detect_wire == W.circuit_red then
-				conn[W.combinator_input_red  ].connect_to(d[detect_wire], false, HIDDEN)
+			if wire == W.circuit_red then
+				ctrl.parameters = AC_NEGATE_EACH_R
+				conn[W.combinator_input_red  ].connect_to(d[wire], false, HIDDEN)
 				conn[W.combinator_input_green].connect_to(conn[W.combinator_output_green], false, HIDDEN)
 			else
+				ctrl.parameters = AC_NEGATE_EACH_G
 				conn[W.combinator_input_red  ].connect_to(conn[W.combinator_output_red], false, HIDDEN)
-				conn[W.combinator_input_green].connect_to(d[detect_wire], false, HIDDEN)
+				conn[W.combinator_input_green].connect_to(d[wire], false, HIDDEN)
 			end
-			return ac
+			
+			acs[wire] = ac
+			return true
 		end
 		
-		if not data.acs then
-			data.acs = {
-				make_combinator(-0.4, -1.5, AC_NEGATE_EACH_R, W.circuit_red),
-				make_combinator(0.4, -1.5, AC_NEGATE_EACH_G, W.circuit_green),
-			}
+		local changed = update_combinator(CR)
+		      changed = update_combinator(CG) or changed
+		if changed then
+			local it = nil
+			it, data.ac1 = next(acs, it)
+			it, data.ac2 = next(acs, it)
 		end
-		
+	end
+	if active and data.ac1 then
 		-- add to update lists
 		local surf = storage.surfaces[display.surface_index]
 		if not surf then
@@ -241,18 +274,13 @@ local function _comp_signal(l, r)
 	return l.count > r.count
 end
 
---local dbg = true
---local function _dbg_update(display, no_change)
---	local pos = display.position
---	
---	local color = no_change and {.2,.2,1} or {.2,1,.2}
---	
---	rendering.draw_line{ from={ x=pos.x-0.45, y=pos.y-0.45 }, to={ x=pos.x+0.45, y=pos.y+0.45 }, color=color, width=2, surface=display.surface, time_to_live=1 }
---	rendering.draw_line{ from={ x=pos.x-0.45, y=pos.y+0.45 }, to={ x=pos.x+0.45, y=pos.y-0.45 }, color=color, width=2, surface=display.surface, time_to_live=1 }
---	
---	rendering.draw_line{ from={ x=pos.x-0.45, y=pos.y-0.45 }, to={ x=pos.x+0.45, y=pos.y+0.45 }, color=color, width=2, surface=display.surface, time_to_live=1, render_mode="chart" }
---	rendering.draw_line{ from={ x=pos.x-0.45, y=pos.y+0.45 }, to={ x=pos.x+0.45, y=pos.y-0.45 }, color=color, width=2, surface=display.surface, time_to_live=1, render_mode="chart" }
---end
+-- TODO: use /editor to look at the exact tick delay my display has
+-- change detection adds no delay, since I'm reading the current+previous signals directly (summed by the engine)
+-- the engine either susm red+green on the API call or has the sum already cached, there's a chance that since the change detector.get_signals returns nil, it's fater than usual
+-- writing the display message may get displayed in the tick, or next tick, I haven't checked
+-- a circuit network might show iron=1 (was 0) on tick=0, a control behavior like a lamp turning on if iron>0 actually lights up one tick later at tick=1, my display matches this! (showing iron=1 at tick=1)
+-- -> either get_signals add the delay, or setting the message, but I belive it should be the message
+-- -> LuaEntity.get_signals supposedly is "current", ie the same as the the in-game gui should show
 
 ---@param data Display
 local function update_messages(data)
@@ -273,7 +301,7 @@ local function update_messages(data)
 	-- I had a decent version of preventing exceeding 500 chars when showing multiple signals, but ran into the fact that the line cutoff still breaks color/font
 	-- so it's pointless to do, I now let the game itself cutoff get_all_signals_text()
 	
-	--if dbg then _dbg_update(display) end
+	--_dbg_update(display)
 	
 	-- cache in case multiple messages exist
 	local all_signals = nil
@@ -287,6 +315,9 @@ local function update_messages(data)
 		--local num_format = precision and string.format("{ [font=default-bold]%%.%df[/font] }", precision) or "{ [font=default-bold]%d[/font] }"
 		local num_format = precision and string.format("{ %%.%df }", precision) or "{ %d }"
 		
+		-- TODO: string.format + regex.gsub sounds close to optimal, but...
+		-- since I'm now polling, it's acceptable to return the wrong result until the poll happens, so I can cache the format string based on the message
+		-- which could remove the regex!
 		return input:gsub("{[^{}]*}", string.format(num_format, count / div), 1)
 	end
 	
@@ -315,6 +346,9 @@ local function update_messages(data)
 		
 		table.sort(all_signals, _comp_signal)
 		
+		-- TODO: I don't know performance characteristics of lua, but creating new strings might be slow
+		-- -> consider optimizing by caching this in storage: /(%d+)f(%d+) -> form/formQ (via polling)
+		
 		-- support fixed point values: /1000f3 => signal=1234567 -> 123.457 (.4567 rounded to .457)
 		-- don't support more variations due to heavy lua regex limitations
 		local divisor, precision = input:match("/(%d+)f(%d+)")
@@ -325,6 +359,9 @@ local function update_messages(data)
 		local form = "%s [%s=%s] "..num_format
 		local formQ = "%s [%s=%s,quality=%s] "..num_format
 		
+		-- the string.format here is needed every tick, but signals tend to not change that much, might be able to cache more here
+		-- -> could cache [%s=%s,quality=%s] entirely, but unclear how what to use as key
+		-- TODO: since items are most common and appear with sig.type=nil, could add fastpath for it instead of formatting in "item", not sur?
 		local text = "{"
 		for _,s in ipairs(all_signals) do
 			local sig = s.signal
@@ -381,8 +418,6 @@ local function update_messages(data)
 end
 
 local function polling(event)
-	--if not storage.polling_radars_cur then return end
-	
 	-- update entire list and thus each entity exactly once every period
 	local period = 90
 	local list = storage.poll_list
@@ -391,6 +426,7 @@ local function polling(event)
 	
 	for i=storage.poll_cur,last do
 		check_display(list[i].entity)
+		--_dbg_update(list[i].entity, true)
 	end
 	
 	if ratio == period then -- end of list reached
@@ -401,20 +437,22 @@ local function polling(event)
 end
 local function optimized_update()
 	-- optimize if no displays are placed
-	if next(storage.all_displays) == nil then
-		storage.updated_last_tick = {}
-		storage.extra_update = {}
-		return
-	end
+	--if next(storage.all_displays) == nil then
+	--	storage.updated_last_tick = {}
+	--	storage.extra_update = {}
+	--	return
+	--end
 	
 	-- collect lists of per-surface chart and alt lists that are observed
 	-- plus lists of any displays that are not in chart or alt but are hovered
 	-- use 'update_lists[obj] = true' to deduplicate
 	local update_lists = {}
 	local extra_update = storage.extra_update
-	for _, player in pairs(game.players) do
-		if not player.connected then goto continue end
-		
+	--for _, player in pairs(game.players) do
+	--	if not player.connected then goto continue end
+	for _, player in pairs(game.connected_players) do
+		-- TODO: optimize via on_player_toggled_alt_mode + ?
+		-- sadly chart mode does not seem to have en event? poll that one infrequently?
 		local surf = storage.surfaces[player.surface_index]
 		if surf then
 			if player.render_mode == defines.render_mode.chart then -- game view or char_zoomed_in render actual entities, chart is map mode
@@ -424,17 +462,22 @@ local function optimized_update()
 			end
 		end
 		
+		-- TODO: This should be optimized via on_selected_entity_changed and on_gui_opened/closed
 		local open_id = player.opened_gui_type == defines.gui_type.entity and player.opened.unit_number or nil
 		
 		local sel_id = player.selected and player.selected.unit_number
 		local data = sel_id and sel_id ~= open_id and storage.all_displays[sel_id] or nil
 		if data then ---@cast sel_id -nil
 			extra_update[sel_id] = data
-		elseif open_id then
+		end
+		
+		-- weird logic, and doens't even work if player 1 has ui open, and player 2 hovers it
+		-- TODO: optimize by just not removing from update list on open, remove this check here, and then just skip if update_messages actually happens?
+		if open_id then
 			extra_update[open_id] = nil
 		end
 		
-		::continue::
+		--::continue::
 	end
 	
 	-- if update list was not updates last tick, bypass change detection
@@ -453,10 +496,11 @@ local function optimized_update()
 				-- Cool optimization: edge detector AC, read combined inputs (current + negated_previous tick signals) if get_signals()=nil, then no change
 				-- Need 2 to catch red and green wires (could usually avoid get_signals if wire connect/disconnect was an event, but unclear how slow 1 API call actually is vs the extra check in lua
 				-- This change detection only works if we also updated this display last tick!
-				local acA = data.acs[1]
-				local acB = data.acs[2]
-				if acA.get_signals(CR, CG) ~= nil or
-				   acB.get_signals(CR, CG) ~= nil then
+				
+				-- TODO: can this still be micro-optimized?
+				-- Check change detector for single or both wires
+				local b = data.ac2
+				if data.ac1.get_signals(CR, CG) or (b and b.get_signals(CR, CG)) then
 					update_messages(data)
 					extra_update[id] = nil
 				--else
@@ -546,7 +590,8 @@ end
 script.on_event(defines.events.on_entity_settings_pasted, on_entity_event) -- source to destination
 
 -- Choose to not react to undo/redo, blueprinting or wire changes even is technically possible via various techniques or libraries
--- as it is very, very complicated and not worth it over polling
+-- but it is very, very complicated and not worth it over polling
+-- I tried blueprint lib + perel + custom undo/redo handlers, and it still did not work correctly
 
 deathrattles[defines.target_type.surface] = function(event)
 	storage.surfaces[event.useful_id] = nil
@@ -589,6 +634,6 @@ script.on_configuration_changed(function(data)
 end)
 
 -- removed to not clutter up /help
---commands.add_command("hexcoder-signal-display-reset", nil, function(command)
---	_reset()
---end)
+commands.add_command("hexcoder-signal-display-reset", nil, function(command)
+	_reset()
+end)
